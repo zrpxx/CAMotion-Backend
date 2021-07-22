1dcb5e02736bbac5ce560dc2b275c35a66e6c24c
Rupeng Zhang <zrpxx@qq.com>
2021-07-22 10:51:49 +0000

    fix CORS

--- main.py
+++ main.py
@@ -44,10 +44,10 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[["*"]],
+    allow_origins=['*'],
     allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+    allow_methods=['*', 'OPTIONS'],
+    allow_headers=['*'],
 )
 
 @app.post("/login/")
