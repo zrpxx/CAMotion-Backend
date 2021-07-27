e1845b4b9736c1341014eefa84c39264727dd28d
刘一手 <55650923+luhuijiao-666@users.noreply.github.com>
2021-07-27 17:40:46 +0800

    cameras bug fix

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
-import websocket
-
-websocket.enableTrace(True)
-ws = websocket.WebSocketApp("ws://127.0.0.1:8000/ws_algo/1")
-
-ws.run_forever()
+# import websocket
+#
+# websocket.enableTrace(True)
+# ws = websocket.WebSocketApp("ws://127.0.0.1:8000/ws_algo/1")
+#
+# ws.run_forever()
